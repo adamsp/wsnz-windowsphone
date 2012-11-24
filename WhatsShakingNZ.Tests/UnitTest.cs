32f79a0996f67f01e5873d0bb22d7155f4520249
Adam Speakman <adam@speakman.net.nz>
2012-11-24 18:24:03 +1300

    Unit tests for the DistanceTool class

---
+++ WhatsShakingNZ.Tests/UnitTest.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhatsShakingNZ.GeonetHelper;
+using System.Text.RegularExpressions;
+using WhatsShakingNZ.Localization;
+
+namespace WhatsShakingNZ.Tests
+{
+    [TestClass]
+    public class UnitTest : SilverlightTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+        }
+    }
+}
